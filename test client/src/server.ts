e054f9f4a0d11ef0d0a014c7741b5365064d04e0
diegofmo0802 <diegofmo0802@gmail.com>
2024-09-15 03:02:42 +0000

    asset name changed "forbuden" to "forbidden"

--- test client/src/server.ts
+++ test client/src/server.ts
@@ -2,7 +2,7 @@ import ServerCore from 'saml.servercore';
 
 const server = new ServerCore(3000);
 
-server.addFile('/.private', 'assets/Forbiden.html');
-server.addFile('/.private/*', 'assets/Forbiden.html');
+server.addFile('/.private', 'assets/Forbidden.html');
+server.addFile('/.private/*', 'assets/Forbidden.html');
 server.addFile('/favicon.ico', 'assets/Logo.svg');
 server.addFolder('/', '../');
